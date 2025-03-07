ed1e3fb9d55afab4ebf447febb552786c21b1788
hari27 <hmsanth@gmail.com>
2025-03-07 11:02:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',  # Fixed spelling of 'Generative'
+    version='0.0.0',
+    author="Harisanth",  # Added missing comma here
+    packages=find_packages(),
+    install_requires=[],
+)
