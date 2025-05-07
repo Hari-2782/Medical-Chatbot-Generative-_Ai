cbe09c9942baa1faaf08f062c27a84c6c8d88d5b
Hari-2782 <hmsanth@gmail.com>
2025-05-07 12:36:06 +0530

    after ollama gemma3 model used

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template,jsonify,request
 from src.helper import download_hugging_face_embedding
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import OpenAI
+# from langchain_openai import OpenAI
+from langchain_community.llms import Ollama
+
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -15,10 +17,10 @@ load_dotenv()
 
 
 PINECONE_API_KEY=os.environ.get("PINECONE_API_KEY")
-OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+# OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
 
 os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
+# os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
 
 
 embeddings=download_hugging_face_embedding()
@@ -33,7 +35,9 @@ docsearch= PineconeVectorStore.from_existing_index(
 retriver=docsearch.as_retriever(search_type="similarity",search_kwargs={"k":3})
 
 
-llm=OpenAI(temperature=0.4,max_tokens=500)
+# llm=OpenAI(temperature=0.4,max_tokens=500)
+llm = Ollama(model="gemma3:1b")  # Replace OpenAI with Ollama
+
 prompt =ChatPromptTemplate.from_messages(
     [
         ("system",system_prompt),
